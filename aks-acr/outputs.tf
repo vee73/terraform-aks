212f809634d75da82cc2f43c031dc98c12da4410
vee73 <veereshsy05@gmail.com>
2023-03-30 16:01:47 +0530

    Update outputs.tf

--- aks-acr/outputs.tf
+++ aks-acr/outputs.tf
@@ -1,14 +1,4 @@
-output "aks_id" {
-  value = azurerm_kubernetes_cluster.aks.id
-}
 
-output "aks_fqdn" {
-  value = azurerm_kubernetes_cluster.aks.fqdn
-}
-
-output "aks_node_rg" {
-  value = azurerm_kubernetes_cluster.aks.node_resource_group
-}
 
 output "acr_id" {
   value = azurerm_container_registry.acr.id
